31b1b8f0ad221080f7d9f282fe085ac61a082598
Daniel Thorpe <danthorpe@me.com>
2015-10-28 12:40:30 +0000

    [MNY-1]: Adds nonesence code with full coverage.

--- Tests/MoneyTests.swift
+++ Tests/MoneyTests.swift
@@ -11,26 +11,8 @@ import XCTest
 
 class MoneyTests: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test__money_value_is_set() {
+        let money = Money()
+        XCTAssertEqual(money.value, 10)
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
